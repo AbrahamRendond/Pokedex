902c8b8ad493590e83b96161d9669089e24949f5
AbrahamRendond <aranita209@gmail.com>
2024-05-08 23:56:33 -0600

    Si

--- public/scripts.js
+++ public/scripts.js
@@ -1,96 +1,72 @@
 document.addEventListener('DOMContentLoaded', function() {
     const ListaPokemon = document.getElementById('lista');
-    const DetallesPokemon = document.getElementById('detalles');
 
     fetch('http://localhost:3000/pokedex')
-        .then(response => response.json())
-        .then(pokemonLista => {
-                    pokemonLista.forEach(pokemon => {
-                        const pokemonIcono = document.createElement('div');
-                        const imagen = pokemon.imagen.substring(0,pokemon.imagen.indexOf(','));
-                        const imagenalt = pokemon.imagen.substring(pokemon.imagen.indexOf(',')+1,pokemon.imagen.length);
-                        pokemonIcono.classList.add('pokemon-card', `generacion-${pokemon.generacion}`);
-                        pokemonIcono.innerHTML = `
-                            <h2>${pokemon.nombre}</h2>
-                            <img src=${imagen} alt=${pokemon.nombre} onerror="this.onerror=null; this.src='${imagenalt}'; this.classList.add('imagen-alt');" />
-                        `;
-                        pokemonIcono.onclick = function(event) {
-                            event.stopPropagation();
-                            Actualizar(pokemon);
-                        };
-                        ListaPokemon.appendChild(pokemonIcono);
-                    });
-                })
-        .catch(error => console.error('Error:', error));
+    .then(response => response.json())
+    .then(pokemonLista => {
+        pokemonLista.forEach(pokemon => {
+            // Separando las URLs de las imágenes
+            const [gifUrl, pngUrl] = pokemon.imagen.split(',');
+
+            const pokemonCard = document.createElement('div');
+            pokemonCard.className = 'col-sm-6 col-md-4 col-lg-3 mb-4';
+
+            // Asignar clase de borde según la generación
+            let borderClass = `generacion-${pokemon.generacion}`;
+            pokemonCard.innerHTML = `
+                <div class="${borderClass}" id="pokemon-card" data-bs-toggle="modal" data-bs-target="#pokemonModal">
+                    <img src="${gifUrl}" onerror="this.src='${pngUrl}'" class="card-img-top" alt="${pokemon.nombre}">
+                    <div class="card-body">
+                        <h5 class="card-title">${pokemon.nombre}</h5>
+                    </div>
+                </div>
+            `;
+            pokemonCard.addEventListener('click', function() {
+                Actualizar(pokemon);
+            });
+            document.getElementById('lista').appendChild(pokemonCard);
+        });
+    })
+    .catch(error => console.error('Error:', error));
 
     function Actualizar(pokemon) {
-
-        const detallesPokemon = document.getElementById('detalles');
 
         const tipos = pokemon.tipo.split(',').map(tipo => {
             const tipoClase = `tipo-${tipo.trim()}`;
             return `<span class="${tipoClase}">${tipo.trim()}</span>`;
-        }).join(', ');
+        }).join(' ');
         const debilidades = pokemon.debilidades.split(',').map(debilidad => {
             const debilidadClase = `tipo-${debilidad.trim()}`;
                 return `<span class="${debilidadClase}">${debilidad.trim()}</span>`;
-        }).join(', ');
-        
-        const imagen = pokemon.imagen.substring(0,pokemon.imagen.indexOf(','));
-        const imagenalt = pokemon.imagen.substring(pokemon.imagen.indexOf(',')+1,pokemon.imagen.length);
-        
-        DetallesPokemon.innerHTML = `
+        }).join(' ');
 
-        <div class="detalle-flex-container">
-        <div class="detalle-imagen">
-            <img src=${imagen} alt=${pokemon.nombre} onerror="this.onerror=null; this.src='${imagenalt}'; this.classList.add('imagen-alt');" />
-        </div>
-        <div class="detalle-info">
-            <h3>${pokemon.nombre} (#${pokemon.numero_pokedex})</h3>
-            <img id="close" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAAB/ElEQVR4nO2au0oDQRSGP7RYAxYiirWgD6CgW1gqqKAvoT6E8QVyKYyl2mujhb6AsdBeREyRShAEi4hiY4wXFiJITLJnZ2dmxyU/TLfznznLtztnzyz01JNVlYGvLuMJ8A3E9Zve3WIHaxPrKsQsGFVgUGMSgVdVEDdYm1jTQF1guqcxkX1BvAYwG9U4LzAOxqqGJJaAT0GsnIq5B9wIzB+BsRhJjAIPgjgVYCDOw9cQBDmNkcixwP8DmCOmpIitK3hvCL1zaJAUsVdgMoLvOPBiGilVxC6BfsLVB5zbQkoVsSzh2rKJlCpi78AMnTUFvNlGShWxWyDT4WZcJ4VUqwpCLEp/ZsKOcG4eC5IiFuzUy7/mLQh374pJpFQRuweGgSHgzhWkVBE7bA7JtQUSkBQx6bCKlCpiTiKliljibynTiCWKlC7EnEBKB2JOINWqokIiibxuU4+Wp+Fhb1dg/guknEPMT8OG6BkoUTIuI3XgctHop6GM9yJ8WAWt0B/NNxfqDGJFISbbbeaWXEHMT0PzwRMiVdfYDsokWRRuCryySRWVvhCpC5dbpl6EJvZExCb2s03ECkIM1hS8120h5guROokR48g0Yp6lo7eRCEdvGZNIrRBfi8J2at4UUrvoU3DUrR2x1PwwUA4xqxn8haMWEvvMQNyeeqKDvgGmxjw+5jmxEQAAAABJRU5ErkJggg==">
-            <p>${pokemon.descripcion}</p>
-            <p>Tipo: ${tipos}</p>
-            <div id="pokemonChart"></div>
-            <p>Debilidades: ${debilidades}</p>
-            <p>Generacion: ${pokemon.generacion}</p>
-        </div>
-        </div>
-         
+        const [gifUrl, pngUrl] = pokemon.imagen.split(',');
+        const modalBody = document.querySelector('#pokemonModal .modal-body');
+        modalBody.innerHTML = `
+            <div class="detalle-flex-container">
+            <div class="detalle-imagen">
+                <img src="${gifUrl}" onerror="this.src='${pngUrl}'" alt="${pokemon.nombre}">
+            </div>
+            <div class="detalle-info">
+                <p>${pokemon.descripcion}</p>
+                <p>Tipo: ${tipos}</p>
+                <p>Debilidades: ${debilidades}</p>
+                <p>Generacion: ${pokemon.generacion}</p>
+                <div id="pokemonChart" style="height: 400px;"></div>
+            </div>
+            </div>
         `;
-
-        DetallesPokemon.style.display = "block";
-
-        document.querySelectorAll('.pokemon-card').forEach(card => {
-            card.addEventListener('mousemove', tiltEffect);
-            card.addEventListener('mouseleave', resetTiltEffect);
-            card.addEventListener('mouseenter', startTiltEffect);
-        });
 
         var myChart = echarts.init(document.getElementById('pokemonChart'));
         var option = {
             tooltip: {},
-            grid: {
-                containLabel: true,
-                left: '10%',
-                right: '10%',
-                bottom: '10%',
-                top: '10%'
-            },
             radar: {
-                name: {
-                    textStyle: {
-                        color: '#fff',
-                        backgroundColor: '#999',
-                        borderRadius: 3,
-                        padding: [3, 5]
-                    }
-                },
                 indicator: [
-                    { name: 'CP Máximo', max: 6500},
-                    { name: 'HP Máximo', max: 500},
-                    { name: 'Ataque', max: 300},
-                    { name: 'Defensa', max: 250},
-                    { name: 'Stamina', max: 200}
+                    { name: 'CP Máximo', max: 6500 },
+                    { name: 'HP Máximo', max: 500 },
+                    { name: 'Ataque', max: 300 },
+                    { name: 'Defensa', max: 250 },
+                    { name: 'Stamina', max: 200 }
                 ]
             },
             series: [{
@@ -103,68 +79,52 @@ document.addEventListener('DOMContentLoaded', function() {
             }]
         };
         myChart.setOption(option);
-
+        $('#pokemonModal').on('shown.bs.modal', function () {
+            var myChart = echarts.init(document.getElementById('pokemonChart'));
+            myChart.setOption(option);
+        });
+        
     }
 
-});
+    document.querySelectorAll('.card').forEach(card => {
+        card.addEventListener('mousemove', tiltEffect);
+        card.addEventListener('mouseleave', resetTiltEffect);
+    });
 
-//Boton de cerrar detalles
-document.addEventListener("click", function(e){
-    const detalles = document.getElementById('detalles');
-    const close= document.getElementById('close')
-    if (!detalles.contains(e.target) || close.contains(e.target) && detalles.style.display !== "none") {
-        detalles.style.display = "none";
-    }
-});
-
-document.addEventListener('keydown', function(e) {
-    const detalles = document.getElementById('detalles');
-    const close= document.getElementById('close')
-    if (e.key === "Escape" && detalles.style.display !== "none") {
-        detalles.style.display = "none";
-    }
 });
 
 //Funcion para buscar pokemon 
 function BuscarPokemon() {
-    var entrada = document.getElementById('buscador');
-    var filtro = (entrada.value.toUpperCase()).normalize("NFD").replace(/[\u0300-\u036f]/g, "");;
-    var listaPokemon = document.getElementById('lista');
-    var pokemonIcono = listaPokemon.getElementsByClassName('pokemon-card');
+    var entrada = document.getElementById('buscador').value.toUpperCase();
+    var lista = document.getElementById('lista');
+    var tarjetas = lista.querySelectorAll('.col-sm-6.col-md-4.col-lg-3.mb-4'); // Asegúrate de que este selector sea correcto
 
-    for (var i = 0; i < pokemonIcono.length; i++) {
-        var nombre = pokemonIcono[i].getElementsByTagName('h2')[0];
-        if (nombre) {
-            var txtValor = nombre.textContent || nombre.innerText;
-            if (txtValor.toUpperCase().indexOf(filtro) > -1) {
-                pokemonIcono[i].style.display = "";
-            } else {
-                pokemonIcono[i].style.display = "none";
-            }
+    for (var i = 0; i < tarjetas.length; i++) {
+        var titulo = tarjetas[i].querySelector('.card-title').textContent.toUpperCase();
+        if (titulo.indexOf(entrada) > -1) {
+            tarjetas[i].style.display = "";
+        } else {
+            tarjetas[i].style.display = "none";
         }
     }
 }
 
 //Funcion para efecto de carta
 function tiltEffect(e) {
-    const rect = this.getBoundingClientRect();
-    const x = e.clientX - rect.left - rect.width / 2;
-    const y = e.clientY - rect.top - rect.height / 2;
-    
-    const maxAngle = 15;
+    const card = e.currentTarget;
+    const rect = card.getBoundingClientRect();
+    const mouseX = e.clientX - rect.left - rect.width / 2;
+    const mouseY = e.clientY - rect.top - rect.height / 2;
 
-    const tiltX = (y / rect.height) * maxAngle * 2;
-    const tiltY = (x / rect.width) * maxAngle * -2;
+    const rotateX = mouseY / rect.height * 10; // Máximo 10 grados de rotación en X
+    const rotateY = mouseX / rect.width * -10; // Máximo 10 grados de rotación en Y
 
-    this.style.transform = `rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale(1.05)`;
+    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
 }
 
 function resetTiltEffect(e) {
-    this.style.transform = 'rotateX(0deg) rotateY(0deg) scale(1)';
-}
-
-function startTiltEffect(e) {
-    this.style.transition = 'transform 0.1s ease-out';
+    const card = e.currentTarget;
+    card.style.transform = 'rotateX(0) rotateY(0)';
 }
 
 //Limpiar contenedor de texto
@@ -173,8 +133,9 @@ document.getElementById('Limpiar').addEventListener('click', function() {
     BuscarPokemon();
 });
 
+// Estado de grabación
 let VozRec;
-let grabando = false; // Estado de grabación
+let grabando = false; 
 
 // Función para cambiar el ícono del micrófono según el estado de grabación y el modo oscuro
 function cambiarIconoMic(grabando) {

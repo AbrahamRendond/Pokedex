a1e02ba3a8b15768ff9af6edd135fbf0038cc96e
AbrahamRendond <aranita209@gmail.com>
2024-05-10 16:16:36 -0600

    Si

--- app.js
+++ app.js
@@ -5,15 +5,24 @@ const mysql = require('mysql');
 // Creación de una nueva aplicación Express
 const app = express();
 
+const port = process.env.PORT || 3000;
+
+const DB_HOST = process.env.DB_HOST || 'localhost'
+const DB_USER = process. env. DB_USER || 'root'
+const DB_PASSWORD = process. env. DB_PASSWORD || ''
+const DB_NAME = process. env. DB_NAME || 'pokedex'
+const DB_PORT = process.env.DB_PORT3306 || 3306
+
 // Configuración del servidor para servir archivos estáticos
 app.use(express.static('public'));
 
 // Configuración de la conexión a la base de datos MySQL
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'pokedex'
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+  database: DB_NAME
 });
 
 // Establece la conexión con la base de datos
@@ -28,6 +37,6 @@ app.get('/pokedex', (req, res) => {
 });
 
 // Inicia el servidor en el puerto 3000
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Servidor escuchando en el puerto 3000');
 });
